838d97c36612977a59788f34abc4c611f3f0646c
fredrik <fredrik.appelberg@scanpix.se>
2011-05-11 17:36:31 +0200

    manages basic parsing

--- inform-htmlizer.el
+++ inform-htmlizer.el
@@ -24,20 +24,29 @@ Carry out switching when the player is Edward: now the player is Roger. Carry ou
 Report switching: say \"You are now [the player].\"   ")
 
 (setq *inform7-tokens* 
-      '(("^\\s*" . (lambda (s) (concat "<div class=\"indent\">")))
-	("^\\(Volume\\|Book\\|Part\\|Chapter\\|Section\\|Table\\).*?$" . (lambda (s) (concat "<div class=\"heading\">" s "</div>")))
-	("$" . (lambda (s) "</div>\n"))
-	("\"" . (lambda (s) (if (eq 'quote (car *inform7-parse-stack*))
-				(progn (pop *inform7-parse-stack*)
-				       "</span>\"")
-			      (progn (push 'quote *inform7-parse-stack*)
-				     "<span class=\"quote\">\""))))
+      '(("^[\t ]+" . (lambda (s) 
+		       (push 'indent *inform7-parse-stack*)
+		       (concat "<div class=\"indent\">")))
+	("\\(Volume\\|Book\\|Part\\|Chapter\\|Section\\|Table\\).*?$" . (lambda (s) (concat "<div class=\"heading\">" s "</div>")))
+	("$" . (lambda (s) 
+		 (if (eq 'indent (car *inform7-parse-stack*))
+		     (progn
+		       (pop *inform7-parse-stack*)
+		       "</div>\n")
+		   "\n")))
+	("\"" . (lambda (s) 
+		  (princ *inform7-parse-stack*)
+		  (if (eq 'quote (car *inform7-parse-stack*))
+		      (progn (pop *inform7-parse-stack*)
+			     "\"</span>")
+		    (progn (push 'quote *inform7-parse-stack*)
+			   "<span class=\"quote\">\""))))
 	("\\[" . (lambda (s) 
-		  (push 'bracket *inform7-parse-stack*)
-		  "<span class=\"bracket\">["))
+		   (push 'bracket *inform7-parse-stack*)
+		   "<span class=\"bracket\">["))
 	("\\]" . (lambda (s) 
-		  (pop *inform7-parse-stack*)
-		  "]</span>"))))
+		   (pop *inform7-parse-stack*)
+		   "]</span>"))))
 
 (defun inform7-next-token (str start)
   (when-let (m (remove-if-not #'identity
@@ -48,21 +57,20 @@ Report switching: say \"You are now [the player].\"   ")
     (reduce (lambda (a b) (if (< (car a) (car b)) a b)) m)))
 
 (defun inform7-htmlize ()
-  (loop for start = 0 then (if (= start end) (1+ start) end) ;; handle zero-length matches
-	while (< start (length *inform7-parse-doc*))
-	for m = (inform7-next-token *inform7-parse-doc* start)
-	while m
-	for beg = (first m)
-	for end = (second m)
-	append (list 
-		(substring *inform7-parse-doc* start beg)
-		(apply (third m) (list (substring  *inform7-parse-doc* beg end))))))
-
-(setq res (inform7-htmlize))
-
-(defun inform7-htmlize (str)
-  (interactive 
-   (list (buffer-substring (region-beginning) (region-end))))
-
+  (interactive)
+  (let ((*inform7-parse-stack* nil)
+	(buf (generate-new-buffer "*inform7-htmlize*")))
+    (switch-to-buffer buf)
+    (nxml-mode)
+    (insert (apply 'concat
+		   (loop for start = 0 then (if (= start end) (1+ start) end) ;; handle zero-length matches
+			 while (< start (length *inform7-parse-doc*))
+			 for m = (inform7-next-token *inform7-parse-doc* start)
+			 while m
+			 for beg = (first m)
+			 for end = (second m)
+			 append (list 
+				 (substring *inform7-parse-doc* start beg)
+				 (apply (third m) (list (substring  *inform7-parse-doc* beg end)))))))))
 
 (provide 'inform-htmlizer)
